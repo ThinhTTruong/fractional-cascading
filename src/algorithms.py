46d2e18e9b029a9cf830d3c2f52a620a69c0f7e0
Thinh Truong <thinhtruong@ThinhTruong.local>
2023-10-10 21:58:24 -0400

    add naive and fractional cascading algorithms

---
+++ src/algorithms.py
@@ -0,0 +1,81 @@
+from node import TreeNode, ListNode
+
+def naive_algorithm(path: list[TreeNode], target: int):
+    result = []
+    for tree_node in path:
+        # binary seach
+        search_result = binary_search_naive(tree_node.node_list, target)
+        result.append(search_result)
+    return result
+
+def binary_search_naive(lst, target):
+    left, right = 0, len(lst) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if lst[mid] == target:
+            return mid
+        elif lst[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1 
+
+    return -1 
+
+def fractional_cascading(path: list[TreeNode], target: int):
+    result = []
+
+    # Binary search on first tree node
+    current_node = path[0]
+    first_index = binary_search_fc(path[0].augmented_list, target)
+    if current_node.augmented_list[first_index].proper[1].value == target:
+        result.append(current_node.augmented_list[first_index].proper[0])
+    else:
+        result.append(-1)
+
+    cur_index = first_index
+    # fractional cascading on other nodes
+    for i in range(len(path)-1):
+        cur_index, res = helper(cur_index, path[i], path[i+1], target)
+        result.append(res)
+
+    return result
+
+def binary_search_fc(lst: list[ListNode], target: int):
+    left, right = 0, len(lst) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if lst[mid].value == target:
+            return mid
+        elif lst[mid].value < target:
+            left = mid + 1
+        else:
+            right = mid - 1 
+
+    return left 
+
+def helper(cur_index: int, cur_node: TreeNode, next_node: TreeNode, target: int):
+    cur_list = cur_node.augmented_list
+    current = cur_list[cur_index]
+    while current:
+        if not current.bridges:
+            current = current.next
+        else:
+            for bridge in current.bridges:
+                if bridge.tree_node == next_node: #step 2
+                    current = bridge #step 3
+                    break
+
+    while current and current.prev.value >= target: #step 4
+        current = current.prev
+    
+    index = next_node.augmented_list.index(current)
+
+    if current.proper and current.proper[1].value == target:
+        return (index, current.proper[0]) # return index of current element in A(v), and index of matching elemnt in C(v)
+
+    return (index, -1)
+
